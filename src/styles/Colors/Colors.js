5005640cbab1cfa6c9eaaabdb2d69a6bb726ff17
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-11-21 09:31:47 -0300

    Adição das cores e do roteamento Stack

---
+++ src/styles/Colors/Colors.js
@@ -0,0 +1,10 @@
+export default {
+  backgroundColor: '#000',
+  lightBacgroundColor: '#1f1f1f',
+  lighterBackgroundColor: '#292929',
+  yellow: '#ffce00',
+  green: '#5cbc29',
+  red: '#cc042a',
+  grey: '#5252525d',
+  line: '#7070705a',
+};
